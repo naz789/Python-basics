761ba80dca362db6bc8fa89544d90d5ba809415c
Nazia Zehan <113630211+naz789@users.noreply.github.com>
2023-07-11 21:13:25 +0530

    Add files via upload
    Some of the very easy an first programs on pythons

---
+++ Python Basics.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ###python basics
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[2]:
+
+
+print(19)
+
+
+# In[3]:
+
+
+print(10.7)
+
+
+# # variable
+
+# In[9]:
+
+
+First_name='Nazia'
+First_name
+
+
+# # Data type
+
+# In[15]:
+
+
+x="love"
+type(x)
+
+
+# In[19]:
+
+
+y=123
+type(y)
+
+
+# In[20]:
+
+
+a=1.222
+type(a)
+
+
+# In[22]:
+
+
+x=100
+type(x)
+x
+
+
+# In[25]:
+
+
+x,y=10,20
+x
+y
+
+
+# In[26]:
+
+
+y,x
+
+
+# ### print your name,age,data type
+
+# In[27]:
+
+
+name="nazia"
+name
+
+
+# In[28]:
+
+
+My_name='nazia'
+type(My_name)
+
+
+# In[29]:
+
+
+age=22
+age
+
+
+# In[30]:
+
+
+age=22
+type(age)
+
+
+# In[ ]:
+
+
+
+
